32edf76c19388f68cfc0ff34a0f92e17560c73f7
GiaTK <giatkt1598@gmail.com>
2026-03-20 18:18:29 +0700

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TranslationAppProvider } from './app/translation-app-context'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { TranslationAppProvider } from "./app/translation-app-context";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <TranslationAppProvider>
-      <App />
-    </TranslationAppProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <TranslationAppProvider>
+    <App />
+  </TranslationAppProvider>,
+);
